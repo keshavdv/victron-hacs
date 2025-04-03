679bb37be088a2b6ef85dce087e827c77bfc15b2
Kai Straube <kaistdd+git@gmail.com>
2025-04-03 16:07:33 +0200

    Added AcCharger, SmartBatteryProtect

--- custom_components/victron_ble/sensor.py
+++ custom_components/victron_ble/sensor.py
@@ -1,4 +1,5 @@
 """Support for Victron ble sensors."""
+
 from __future__ import annotations
 
 import logging
@@ -24,8 +25,9 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.sensor import sensor_device_info_to_hass_device_info
 from sensor_state_data.data import SensorUpdate
 from sensor_state_data.units import Units
-from victron_ble.devices.base import ChargerError, OffReason, OperationMode
+from victron_ble.devices.base import AlarmReason, ChargerError, OffReason, OperationMode
 from victron_ble.devices.battery_monitor import AuxMode
+from victron_ble.devices.smart_battery_protect import OutputState
 
 from .const import DOMAIN
 from .device import VictronSensor
@@ -126,6 +128,24 @@ SENSOR_DESCRIPTIONS: Dict[Tuple[SensorDeviceClass, Optional[Units]], Any] = {
         native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
+    (
+        VictronSensor.OUTPUT_CURRENT,
+        Units.ELECTRIC_CURRENT_AMPERE,
+    ): SensorEntityDescription(
+        key=VictronSensor.OUTPUT_CURRENT,
+        device_class=SensorDeviceClass.CURRENT,
+        native_unit_of_measurement=Units.ELECTRIC_CURRENT_AMPERE,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (
+        VictronSensor.OUTPUT_POWER,
+        Units.POWER_WATT,
+    ): SensorEntityDescription(
+        key=VictronSensor.OUTPUT_POWER,
+        device_class=SensorDeviceClass.POWER,
+        native_unit_of_measurement=Units.POWER_WATT,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
     (SensorDeviceClass.BATTERY, Units.PERCENTAGE): SensorEntityDescription(
         key=f"{SensorDeviceClass.BATTERY}_{Units.PERCENTAGE}",
         device_class=SensorDeviceClass.BATTERY,
@@ -159,6 +179,32 @@ SENSOR_DESCRIPTIONS: Dict[Tuple[SensorDeviceClass, Optional[Units]], Any] = {
         device_class=SensorDeviceClass.VOLTAGE,
         native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
         state_class=SensorStateClass.MEASUREMENT,
+    ),
+    (VictronSensor.CONSUMED_ENERGY, Units.ENERGY_WATT_HOUR): SensorEntityDescription(
+        key=VictronSensor.CONSUMED_ENERGY,
+        device_class=SensorDeviceClass.ENERGY,
+        native_unit_of_measurement=Units.ENERGY_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+    ),
+    (VictronSensor.ALARM_REASON, None): SensorEntityDescription(
+        key=VictronSensor.ALARM_REASON,
+        device_class=SensorDeviceClass.ENUM,
+        options=[x.lower() for x in AlarmReason._member_names_],
+    ),
+    (VictronSensor.WARNING_REASON, None): SensorEntityDescription(
+        key=VictronSensor.WARNING_REASON,
+        device_class=SensorDeviceClass.ENUM,
+        options=[x.lower() for x in AlarmReason._member_names_],
+    ),
+    (VictronSensor.DEVICE_STATE, None): SensorEntityDescription(
+        key=VictronSensor.DEVICE_STATE,
+        device_class=SensorDeviceClass.ENUM,
+        options=[x.lower() for x in OperationMode._member_names_],
+    ),
+    (VictronSensor.OUTPUT_STATE, None): SensorEntityDescription(
+        key=VictronSensor.OUTPUT_STATE,
+        device_class=SensorDeviceClass.ENUM,
+        options=[x.lower() for x in OutputState._member_names_],
     ),
 }
 
